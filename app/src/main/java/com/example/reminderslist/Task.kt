299a1f35b94f6c20f36bfd3a44e5c536108e1a98
IgniteCoders <theboom.kajas@gmail.com>
2025-03-27 09:42:32 +0100

    Implemented SQLite and added Tasks table with the DAO.

---
+++ app/src/main/java/com/example/reminderslist/Task.kt
@@ -0,0 +1,17 @@
+package com.example.reminderslist
+
+class Task (
+    var id: Long,
+    var title: String,
+    var done: Boolean
+) {
+
+
+    companion object {
+        const val TABLE_NAME = "Tasks"
+
+        const val COLUMN_NAME_ID = "id"
+        const val COLUMN_NAME_TITLE = "title"
+        const val COLUMN_NAME_DONE = "done"
+    }
+}
